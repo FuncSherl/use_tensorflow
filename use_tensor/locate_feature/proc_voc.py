dbbe18f06603122760ff541400f6b6da97a90ce8
functionxu123 <1229853312@qq.com>
2018-09-24 01:14:29 +0800

    add proc_voc

--- use_tensor/locate_feature/proc_voc.py
+++ use_tensor/locate_feature/proc_voc.py
@@ -1,9 +1,46 @@
 #coding:utf-8
 '''
-Created on 2018Äê9ÔÂ24ÈÕ
+Created on 2018ï¿½ï¿½9ï¿½ï¿½24ï¿½ï¿½
 
 @author: sherl
 '''
+from xml.dom import minidom
+import cv2,os
+import os.path as op
+
+
+def show_objects(dir_xml):
+    doc = minidom.parse(dir_xml)
+    root = doc.documentElement
+    
+    filename=root.getElementsByTagName('filename')[0].childNodes[0].data
+    
+    file_dir=op.join(op.split(dir_xml)[0],'../JPEGImages/'+filename)
+    print (filename)
+    
+    img=cv2.imread(file_dir)
+    
+    objects=root.getElementsByTagName('object')
+    for i in objects:
+        name=i.getElementsByTagName('name')[0].childNodes[0].data
+        bndbox=i.getElementsByTagName('bndbox')[0]
+        xmin=int(bndbox.getElementsByTagName('xmin')[0].childNodes[0].data)
+        ymin=int(bndbox.getElementsByTagName('ymin')[0].childNodes[0].data)
+        xmax=int(bndbox.getElementsByTagName('xmax')[0].childNodes[0].data)
+        ymax=int(bndbox.getElementsByTagName('ymax')[0].childNodes[0].data)
+        
+        cv2.rectangle(img, (xmin, ymin), (xmax, ymax), [255,0,0], 3)
+        
+        cv2.putText(img, name, (xmin, ymin), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255))
+    
+    cv2.imshow('test', img)
+    cv2.waitKey()
+    
 
 if __name__ == '__main__':
-    pass
+    rootdir='F:\DL_datasets\PascalVOC2012\VOC_2012_all\VOC2012_trainval'
+    annotation_dir=op.join(rootdir, 'Annotations')
+    for i in os.listdir(annotation_dir):
+        tep=op.join(annotation_dir, i)
+        show_objects(tep)
+    
