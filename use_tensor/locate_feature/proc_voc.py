d4ce263a1811c26054daff50c0c835fbdd665c0e
functionxu123 <1229853312@qq.com>
2018-09-24 19:22:13 +0800

    x

--- use_tensor/locate_feature/proc_voc.py
+++ use_tensor/locate_feature/proc_voc.py
@@ -43,12 +43,45 @@ def show_objects(dir_xml):
     cv2.imshow('test', img)
     cv2.waitKey()
     
+    
+def get_label_file(dir_labels):
+    '''
+    input:dir to these label files
+    out:a map with key->class name, val->a list of file names in this class,for example:
+        aeroplane ['2008_000021', '2008_000033']
+        bicycle ['2008_000036', '2008_000090']
+        bird ['2008_000054', '2008_000095']
+        boat ['2008_000007', '2008_000036']
+    '''
+    ret={}
+    for label,i in enumerate(classes):
+        ret[i]=[]
+        fname=i+'_trainval.txt'
+        tep=op.join(dir_labels, fname)
+        with open(tep, 'r') as f:
+            lin=f.readlines()
+            for j in lin:
+                t=j.strip().split()
+                if int(t[-1])==1:
+                    ret[i].append(t[0].strip())
+    '''
+    cnt=0
+    for i in ret:               
+        cnt+=len(ret[i])
+    print (cnt)
+    '''
+    return ret
+        
+    
 
 if __name__ == '__main__':
     rootdir='F:\DL_datasets\PascalVOC2012\VOC_2012_all\VOC2012_trainval'
     testdir='F:\DL_datasets\PascalVOC2012\VOC_2012_all\VOC2012_test'
     
     annotation_dir=op.join(rootdir, 'Annotations')
+    label_dir=op.join(rootdir, 'ImageSets\Main')
+    get_label_file(label_dir)
+    
     for i in os.listdir(annotation_dir):
         tep=op.join(annotation_dir, i)
         show_objects(tep)
