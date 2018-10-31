9a79c9d29872c57c7791761ad8e7907e1ea4f325
functionxu123 <1229853312@qq.com>
2018-10-31 18:46:12 +0800

    asdf

--- use_tensor/locate_feature/proc_voc.py
+++ use_tensor/locate_feature/proc_voc.py
@@ -196,7 +196,7 @@ def read_tfrecord_batch(tfdir,batchsize=32):
     
     dataset=dataset.map(parse,num_parallel_calls=4)#注意把值回赋给dataset
     
-    dataset=dataset.batch(batchsize).shuffle(batchsize*16)
+    dataset=dataset.batch(batchsize).shuffle(batchsize*10)
     #print("dataset.output_shapes",dataset.output_shapes)
     
     iterator = dataset.make_one_shot_iterator()
