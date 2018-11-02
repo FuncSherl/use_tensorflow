a428e7a19360226425cd6bafcfaeb157e1b4eb7f
functionxu123 <1229853312@qq.com>
2018-11-02 20:08:26 +0800

    asdf

--- use_tensor/locate_feature/proc_voc.py
+++ use_tensor/locate_feature/proc_voc.py
@@ -273,11 +273,11 @@ message Feature{
 '''
 def preprocess_img(image,outlen=224):
     #这里将图片变成224大小的，但是如果用crop可能导致切出来的图片里没有目标物体
-    #print (image)
+    '''
     image=tf.image.resize_images(image, (230,230))
     image=tf.cast(image, dtype=tf.uint8)
-    #print (image)
-    #image = tf.image.resize_image_with_crop_or_pad(image, 230, 230)
+    '''
+    image = tf.image.resize_image_with_crop_or_pad(image, 230, 230)
     image = tf.random_crop(image, [outlen, outlen, 3])
     image = tf.image.random_flip_left_right(image)
     
