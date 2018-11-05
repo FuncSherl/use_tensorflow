0cd7d880c04f5e0509f9b95a9d54c51e32591929
functionxu123 <1229853312@qq.com>
2018-11-05 22:18:46 +0800

    fhj

--- use_tensor/locate_feature/proc_voc.py
+++ use_tensor/locate_feature/proc_voc.py
@@ -10,7 +10,7 @@ from datetime import datetime
 import os.path as op
 from PIL import Image
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 
 import tensorflow as tf
@@ -321,6 +321,24 @@ def read_tfrecord_batch(tfdir,batchsize=32):
 
     return image_batch, label_batch
     
+def voc_ap(rec, prec, use_07_metric=False):
+    if use_07_metric:
+        # 11 point metric
+        ap = 0.
+        for t in np.arange(0., 1.1, 0.1):
+            if np.sum(rec >= t) == 0:
+                p = 0
+            else:
+                p = np.max(prec[rec >= t])
+            ap = ap + p / 11.
+    else:
+        mrec = np.concatenate(([0.], rec, [1.]))
+        mpre = np.concatenate(([0.], prec, [0.]))
+        for i in range(mpre.size - 1, 0, -1):
+            mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
+        i = np.where(mrec[1:] != mrec[:-1])[0]
+        ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+    return ap
     
     
 
