7954d2bdd8d0b3cf427596e0faf16c02acba094c
functionxu123 <1229853312@qq.com>
2018-12-11 01:06:07 +0800

    asdf

--- use_tensor/GAN_HW_LEEHONGYI/HW3_1_GAN_Network.py
+++ use_tensor/GAN_HW_LEEHONGYI/HW3_1_GAN_Network.py
@@ -29,16 +29,36 @@ class GAN_Net:
     def __init__(self, sess):
         self.sess=sess
         self.tf_inimg=anime_data.read_tfrecord_batch( batchsize=batchsize)  #tensor的img输入
+        self.training_G=tf.placeholder(tf.bool)
+        self.training_D=tf.placeholder(tf.bool)
         
         #两个placeholder， img和noise
         self.noise_pla=tf.placeholder(tf.float32, [batchsize, noise_size], name='noise_in')
         self.imgs_pla = tf.placeholder(tf.float32, [batchsize, img_size, img_size, 3], name='imgs_in')
         
     def get_noise(self):
-        
+        return np.random.random([batchsize, noise_size])
         
     
     def Generator_net(self, noise):
+        # fc1
+        with tf.name_scope('fc1') as scope:
+            shape = int(np.prod(self.pool5.get_shape()[1:]))#除去batch维度，剩下的乘积，用于flatten原来的二维featuremap
+            
+            
+            
+            fc1w = tf.Variable(tf.truncated_normal([shape, 4096],
+                                                         dtype=tf.float32,
+                                                         stddev=1e-1), name='weights')
+            fc1b = tf.Variable(tf.constant(1.0, shape=[4096], dtype=tf.float32),
+                                 trainable=True, name='biases')
+            pool5_flat = tf.reshape(self.pool5, [-1, shape])
+            fc1l = tf.nn.bias_add(tf.matmul(pool5_flat, fc1w), fc1b)
+            self.fc1 = tf.nn.relu(fc1l)
+            self.parameters += [fc1w, fc1b]
+        
+        #dropout1
+        self.fc1=tf.cond(self.training, lambda: tf.nn.dropout(self.fc1, self.dropout), lambda: self.fc1)
         pass
     
     def Discriminator_net(self, imgs):
