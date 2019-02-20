02f4b2d9bd0bd184f8a6555fd86907021fa1ad12
root <root@sherl-desktop>
2019-02-20 19:07:14 +0800

    dgfd

--- use_tensor/GAN_slomo/GAN_Network_slomo.py
+++ use_tensor/GAN_slomo/GAN_Network_slomo.py
@@ -479,7 +479,7 @@ if __name__ == '__main__':
             real,fake=gan.evla_D_once(1)
             print ('once prob of real/fake:',real,fake)
             
-            print ('time used:',time.time()-stt,' to be ',1.0/(time.time()-stt),' iters/s', ' left time:',(time.time()-stt)*(maxstep-i))
+            print ('time used:',time.time()-stt,' to be ',1.0/(time.time()-stt),' iters/s', ' left time:',(time.time()-stt)*(maxstep-i)/60/60,' hours')
             
         
         print ('Training done!!!-->time used:',(time.time()-begin_t),'s = ',(time.time()-begin_t)/60,' min')
