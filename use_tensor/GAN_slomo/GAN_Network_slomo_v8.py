0ccbbb879415336f3e0e896760279f13f5f86d88
functionxu123 <1229853312@qq.com>
2019-04-03 20:08:05 +0800

    sdfsdf

--- use_tensor/GAN_slomo/GAN_Network_slomo_v8.py
+++ use_tensor/GAN_slomo/GAN_Network_slomo_v8.py
@@ -73,7 +73,7 @@ G_group_img_num=3
 img_channel=3
 eval_step=int (test_size/batchsize/G_group_img_num)
 
-logdir="./logs_v7/GAN_"+TIMESTAMP+('_base_lr-%f_batchsize-%d_maxstep-%d'%(base_lr,batchsize, maxstep))
+logdir="./logs_v8/GAN_"+TIMESTAMP+('_base_lr-%f_batchsize-%d_maxstep-%d'%(base_lr,batchsize, maxstep))
 
 bigimgsdir=op.join(logdir, 'randomimgs')
 if not op.exists(bigimgsdir): os.makedirs(bigimgsdir)
