f9766d0fe83667c2f398d69b7add8ebd49b326b7
functionxu123 <1229853312@qq.com>
2019-09-11 22:31:36 +0800

    sdaf

--- use_tensor/LSTM_test/min-char-rnn.py
+++ use_tensor/LSTM_test/min-char-rnn.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 """
 get from other's gist:https://gist.github.com/d4dee566867f8291f086.git
 --change it to python3
@@ -12,7 +13,8 @@ data = open('input.txt', 'r').read()  # should be simple plain text file
 chars = list(set(data))
 data_size, vocab_size = len(data), len(chars)
 print ('data has %d characters, %d unique.' % (data_size, vocab_size))
-char_to_ix = {ch: i for i, ch in enumerate(chars)}
+print (chars)
+char_to_ix = {ch: i for i, ch in enumerate(chars)}  #这里是两个map映射，一个将char映射到index，一个反过来
 ix_to_char = {i: ch for i, ch in enumerate(chars)}
 
 # hyperparameters
@@ -38,7 +40,7 @@ def lossFun(inputs, targets, hprev):
     hs[-1] = np.copy(hprev)
     loss = 0
     # forward pass
-    for t in xrange(len(inputs)):
+    for t in range(len(inputs)):
         xs[t] = np.zeros((vocab_size, 1))  # encode in 1-of-k representation
         xs[t][inputs[t]] = 1
         hs[t] = np.tanh(np.dot(Wxh, xs[t]) + np.dot(Whh,
@@ -52,7 +54,7 @@ def lossFun(inputs, targets, hprev):
     dWxh, dWhh, dWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
     dbh, dby = np.zeros_like(bh), np.zeros_like(by)
     dhnext = np.zeros_like(hs[0])
-    for t in reversed(xrange(len(inputs))):
+    for t in reversed(range(len(inputs))):
         dy = np.copy(ps[t])
         # backprop into y. see
         # http://cs231n.github.io/neural-networks-case-study/#grad if
@@ -69,7 +71,7 @@ def lossFun(inputs, targets, hprev):
         dhnext = np.dot(Whh.T, dhraw)
     for dparam in [dWxh, dWhh, dWhy, dbh, dby]:
         # clip to mitigate exploding gradients
-        np.clip(dparam, -5, 5, out=dparam)
+        np.clip(dparam, -5, 5, out = dparam)
     return loss, dWxh, dWhh, dWhy, dbh, dby, hs[len(inputs) - 1]
 
 
@@ -81,11 +83,11 @@ def sample(h, seed_ix, n):
     x = np.zeros((vocab_size, 1))
     x[seed_ix] = 1
     ixes = []
-    for t in xrange(n):
+    for t in range(n):
         h = np.tanh(np.dot(Wxh, x) + np.dot(Whh, h) + bh)
         y = np.dot(Why, h) + by
         p = np.exp(y) / np.sum(np.exp(y))
-        ix = np.random.choice(range(vocab_size), p=p.ravel())
+        ix = np.random.choice(range(vocab_size), p = p.ravel())
         x = np.zeros((vocab_size, 1))
         x[ix] = 1
         ixes.append(ix)
@@ -99,17 +101,17 @@ smooth_loss = -np.log(1.0 / vocab_size) * seq_length  # loss at iteration 0
 while True:
     # prepare inputs (we're sweeping from left to right in steps seq_length
     # long)
-    if p + seq_length + 1 >= len(data) or n == 0:
+    if p + seq_length + 1 >= len(data) or n == 0:  # 当数据读到尽头的时候就重头开始
         hprev = np.zeros((hidden_size, 1))  # reset RNN memory
         p = 0  # go from start of data
-    inputs = [char_to_ix[ch] for ch in data[p:p + seq_length]]
-    targets = [char_to_ix[ch] for ch in data[p + 1:p + seq_length + 1]]
+    inputs = [char_to_ix[ch] for ch in data[p:p + seq_length]]  # 这里的data与GT其实就是错位1的字符串
+    targets = [char_to_ix[ch] for ch in data[p + 1:p + seq_length + 1]] #GT
 
     # sample from the model now and then
-    if n % 100 == 0:
+    if n % 100 == 0:  #每100次测试一次，将input data中的第一个字符输入，后面依次向下生成200个
         sample_ix = sample(hprev, inputs[0], 200)
         txt = ''.join(ix_to_char[ix] for ix in sample_ix)
-        print ('----\n %s \n----' % (txt, ))
+        print ('----\n %s \n----' % (txt,))
 
     # forward seq_length characters through the net and fetch gradient
     loss, dWxh, dWhh, dWhy, dbh, dby, hprev = lossFun(inputs, targets, hprev)
@@ -127,3 +129,18 @@ while True:
 
     p += seq_length  # move data pointer
     n += 1  # iteration counter
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
