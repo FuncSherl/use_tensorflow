4977e1455f59735c8f419cee5da5718027603c39
functionxu123 <1229853312@qq.com>
2019-11-29 20:42:57 +0800

    asdf

--- use_tensor/GAN_slomo/superslomo_tensorflow_LSTM_withoutshape_360p.py
+++ use_tensor/GAN_slomo/superslomo_tensorflow_LSTM_withoutshape_360p.py
@@ -305,14 +305,14 @@ class SuperSlomo:
                 self.first_L1_loss_interframe, self.first_ssim, self.first_psnr, self.second_GAN_loss_mean_D1=self.loss_cal_all()
                 
         #训练G的总loss
-        self.G_loss_all=180 * self.second_L1_loss_interframe + 65 *  self.first_warp_loss  + 0.05 * self.second_contex_loss \
+        self.G_loss_all=180 * self.second_L1_loss_interframe + 65 *  self.first_warp_loss  + 100 * self.second_contex_loss \
                     +self.second_global_var_loss_all\
-                    + self.second_GAN_loss_mean_D1*0.01   
+                    + self.second_GAN_loss_mean_D1* (self.global_step*1.0/maxstep*2 )
         self.G_loss_all=tf.identity(self.G_loss_all, name="G_loss_all")
         print ("self.G_loss_all:", self.G_loss_all) #G_loss_all:0
         #后面用于finetune时使用的的损失
-        self.G_loss_all_for_finetune=180 * self.second_L1_loss_interframe + 65 *  self.first_warp_loss  + 0.05 * self.second_contex_loss \
-                    +self.second_global_var_loss_all
+        self.G_loss_all_for_finetune=180 * self.second_L1_loss_interframe + 65 *  self.first_warp_loss +self.second_global_var_loss_all
+        # + 0.05 * self.second_contex_loss 
         self.G_loss_all_for_finetune=tf.identity(self.G_loss_all_for_finetune, name="G_loss_all_for_finetune")
         print ("self.G_loss_all_for_finetune:", self.G_loss_all_for_finetune) #G_loss_all_for_finetune:0
 
